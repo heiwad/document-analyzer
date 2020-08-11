673770fd9fa28a29b5400193b0c1e2f89ce4c4ac
Heiwad Osman <heiwad@amazon.com>
2020-08-11 06:31:22 +0000

    Squashed commit of the following
    commit 2f60e8ee 
    Author: Heiwad Osman <heiwad@amazon.com> 
    Date: Tue Aug 11 2020 02:17:55 GMT-0400 (Eastern Daylight Time) 
    
        Refactor
    
    
    commit 814748f2 
    Author: Heiwad Osman <heiwad@amazon.com> 
    Date: Mon Aug 10 2020 17:54:44 GMT-0400 (Eastern Daylight Time) 
    
        Updates

--- DocumentAnalyzerFunction/src/main/java/documentanalyzer/App.java
+++ DocumentAnalyzerFunction/src/main/java/documentanalyzer/App.java
@@ -1,12 +1,10 @@
-package helloworld;
+package documentanalyzer;
 
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,12 +12,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.amazonaws.services.lambda.runtime.events.S3Event;
-
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
 
-
 import com.amazonaws.services.textract.model.S3Object;
-
 import com.amazonaws.services.textract.AmazonTextract;
 import com.amazonaws.services.textract.AmazonTextractClientBuilder;
 import com.amazonaws.services.textract.model.*;
@@ -31,9 +26,7 @@ import com.amazonaws.services.comprehend.model.BatchDetectEntitiesRequest;
 import com.amazonaws.services.comprehend.model.BatchDetectEntitiesResult;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.BatchWriteItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.TableWriteItems;
@@ -50,17 +43,16 @@ public class App implements RequestHandler<S3Event, String> {
     private static String TABLE_NAME;
     private static String TABLE_HASH;
     private static String TABLE_RANGE;
+    private static int MAX_TEXTRACT_BATCH_SIZE = 25;
+    private static int MAX_DYNAMODB_BATCH_SIZE = 100;
 
     public String handleRequest(S3Event s3event, Context context) {
             TABLE_NAME = System.getenv("TABLE_NAME");
             TABLE_HASH = System.getenv("TABLE_HASH");
             TABLE_RANGE = System.getenv("TABLE_RANGE");
 
-      //  try {
             S3EventNotificationRecord record = s3event.getRecords().get(0);
-            
             String srcBucket = record.getS3().getBucket().getName();
-
             // Object key may have spaces or unicode non-ASCII characters.
             String srcKey = record.getS3().getObject().getUrlDecodedKey();
 
@@ -78,31 +70,34 @@ public class App implements RequestHandler<S3Event, String> {
                 return "";
             }
 
+            System.out.println("Invoked with valid document: " + srcBucket + "/" + srcKey);
+
             // Use Textract to process the document
             List<Block> blocks = extractText(srcBucket, srcKey);
 
-            // Convert the text blocks into strings
-            List<List<String>> processed = getText(blocks);
-            List<String> ids = processed.get(0);
-            List<String> text = processed.get(1);
+            // Convert the blocks for lines of text into strings
+            List<String> ids = new ArrayList<String>();
+            List<String> text = new ArrayList<String>();   
+            blocks.forEach(block->{
+                ids.add(block.getId());
+                text.add(block.getText());
+            });
 
+            
             //Use Comprehend to process the text.
             List<BatchDetectEntitiesItemResult> entities = getEntities(text);
 
-            
 
-            System.out.println(entities.toString());
-
+            System.out.println("Saving analysis to dynamodb");
             writeDocumentToDynamoDB(ids, text, entities, srcKey);
 
         return srcKey;    
-   //     } catch (IOException e) {
-   //         throw new RuntimeException(e);
-   //     }
+
     }
 
     private List<Block> extractText(String bucketName, String documentName) {
-
+        System.out.println("Using textract to identify text in the document.");
+            
         S3Object document = new S3Object().withBucket(bucketName).withName(documentName);
 
         AmazonTextract client = AmazonTextractClientBuilder.defaultClient();
@@ -110,40 +105,45 @@ public class App implements RequestHandler<S3Event, String> {
                 .withDocument(new Document().withS3Object(document));
 
         DetectDocumentTextResult result = client.detectDocumentText(request);
-        return result.getBlocks();
 
-    }
-
-    private List<List<String>> getText(List<Block> blocks) {
-
-        List<String> ids = new ArrayList<String>();
-        List<String> text = new ArrayList<String>();
-
-        blocks.forEach((block) -> {
-            ids.add(block.getId());
-            text.add(block.getText());
+        // Keep the non-empty lines of text and ignore the individual words
+        List<Block> filteredResults = new ArrayList<Block>();
+        result.getBlocks().forEach(block->{
+            if (block.getBlockType().equals("LINE") && !(block.getText()== null)){
+                filteredResults.add(block);
+            }
         });
 
-        List<List<String>> processed = new ArrayList<List<String>>();
-        processed.add(ids);
-        processed.add(text);
-        return processed;
+        return filteredResults;
 
     }
+    
 
     private List<BatchDetectEntitiesItemResult> getEntities(List<String> text) {
-
-        AmazonComprehend comprehendClient = AmazonComprehendClientBuilder.standard().build();
-
-        // ISSUE: Hard-coded the language to english. Jon should catch this.
-        BatchDetectEntitiesRequest batchDetectEntitiesRequest = new BatchDetectEntitiesRequest().withTextList(text)
-                .withLanguageCode("en");
-        BatchDetectEntitiesResult batchDetectEntitiesResult = comprehendClient
-                .batchDetectEntities(batchDetectEntitiesRequest);
         
-        //ISSUE: Batch detect could have some failed entries but I didn't check and retry.
-        List<BatchDetectEntitiesItemResult> results = batchDetectEntitiesResult.getResultList();
+        System.out.println("Using comprehend to process " + String.valueOf(text.size()) + " entities.");
+        
+        AmazonComprehend comprehendClient = AmazonComprehendClientBuilder.standard().build();
+        List<BatchDetectEntitiesItemResult> results = new ArrayList<BatchDetectEntitiesItemResult>();
 
+        // Detect Entities for each line of text in batches of up to 25
+        for (int current = 0; current < text.size();) {
+            
+            int batchSize = Math.min(text.size()-current, MAX_TEXTRACT_BATCH_SIZE);
+            List<String> batch = text.subList(current, current + batchSize);
+            
+            BatchDetectEntitiesRequest batchDetectEntitiesRequest = new BatchDetectEntitiesRequest().withTextList(batch)
+                .withLanguageCode("en");
+
+            BatchDetectEntitiesResult batchDetectEntitiesResult = comprehendClient
+                .batchDetectEntities(batchDetectEntitiesRequest);
+
+            results.addAll(batchDetectEntitiesResult.getResultList());
+            current = current + batchSize;
+            
+        } 
+        System.out.println("Processed entities");
+        System.out.println(results);
         return results;
 
     }
@@ -154,43 +154,52 @@ public class App implements RequestHandler<S3Event, String> {
         AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
         DynamoDB dynamoDB = new DynamoDB(ddb);
 
-        TableWriteItems tableWriteItems = new TableWriteItems(TABLE_NAME);
+
+        //Build up item objects for the current document
+        List<Item> items = new ArrayList<Item>();       
+        for (int i = 0; i < ids.size(); i++) {
+
+            //Build list of detected entities for each item
+            List<Map<String,String>> itemEntityList = new ArrayList<Map<String,String>>();
+
+            entities.get(i).getEntities().forEach(entity-> {
+                Map<String,String> itemEntity = new HashMap<String, String>();
+                
+                itemEntity.put("text",entity.getText());
+                itemEntity.put("type", entity.getType());
+                itemEntity.put("score", String.valueOf(entity.getScore()));
+                itemEntityList.add(itemEntity);
+
+            });
+
+            Item item = new Item()
+            .withPrimaryKey(TABLE_HASH, srcKey, TABLE_RANGE, ids.get(i))
+            .withString("text",text.get(i))
+            .withList("entities",itemEntityList);
             
-            //Looping Issue: Iterating over 3 lists but only checking the length of one of the lists!!
-            for (int i = 0; i < ids.size(); i++) {
-                
-                //Build list of detected entities for each item
-                List<Map<String,String>> itemEntityList = new ArrayList<Map<String,String>>();
+            items.add(item);
+        
+        }
 
-                entities.get(i).getEntities().forEach(entity-> {
-                    Map<String,String> itemEntity = new HashMap<String, String>();
-                    
-                    itemEntity.put("text",entity.getText());
-                    itemEntity.put("type", entity.getType());
-                    itemEntity.put("score", String.valueOf(entity.getScore()));
-
-                    itemEntityList.add(itemEntity);
-
-                });
-
-                Item item = new Item()
-                .withPrimaryKey(TABLE_HASH, srcKey, TABLE_RANGE, ids.get(i))
-                .withString("text",text.get(i))
-                .withList("entities",itemEntityList);
-                
+        // Write items to DynamoDB using batches of up to 100           
+        for (int current = 0; current < ids.size();) {
+            
+            int batchSize = Math.min(ids.size()-current, MAX_DYNAMODB_BATCH_SIZE);
+            List<Item> batch = items.subList(current, current + batchSize);
+            TableWriteItems tableWriteItems = new TableWriteItems(TABLE_NAME);
+            
+            batch.forEach(item->{
                 tableWriteItems.addItemToPut(item);
+            });
+            
+            dynamoDB.batchWriteItem(tableWriteItems);
+            current = current + batchSize;
 
-            }
+        }
 
-            BatchWriteItemOutcome outcome = dynamoDB.batchWriteItem(tableWriteItems);
-            System.out.println(outcome.toString());
-            //Did not check results of Batch Write - some of the writes could have failed. I need to rety.
-            return;
-
+        return;
 
     }
-
-
 
 }
 
